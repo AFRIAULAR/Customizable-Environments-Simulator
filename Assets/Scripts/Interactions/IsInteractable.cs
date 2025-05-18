06e9ac1515267bd780b35f1384533ea3081730a0
AFRIAULAR <aularafrica@gmail.com>
2025-05-18 17:15:56 -0300

    Folders & Appearance Joystick

---
+++ Assets/Scripts/Interactions/IsInteractable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsInteractable : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("I'm interactable!");
+        //Buscar como hacerlo con tags
+    }
+}
